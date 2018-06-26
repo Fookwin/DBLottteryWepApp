96208d715f233c8dc0d3f032e45aa8e6b3a337c2
fookwin <fookwin@126.com>
2018-06-26 18:32:03 +0800

    add time countown panel and background

--- public/js/dashboard/ng-home-view-ctrl.js
+++ public/js/dashboard/ng-home-view-ctrl.js
@@ -1,30 +1,30 @@
-$(document).ready(function(){
-    var clickEvent = false;
-	$('#myCarousel').carousel({
-		interval:   4000	
-	}).on('click', '.list-group li', function() {
-			clickEvent = true;
-			$('.list-group li').removeClass('active');
-			$(this).addClass('active');		
-	}).on('slid.bs.carousel', function(e) {
-		if(!clickEvent) {
-			var count = $('.list-group').children().length -1;
-			var current = $('.list-group li.active');
-			current.removeClass('active').next().addClass('active');
-			var id = parseInt(current.data('slide-to'));
-			if(count == id) {
-				$('.list-group li').first().addClass('active');	
-			}
-		}
-		clickEvent = false;
-	});
-});
-$(window).load(function() {
-    var boxheight = $('#myCarousel .carousel-inner').innerHeight();
-    var itemlength = $('#myCarousel .item').length;
-	var triggerheight = Math.round(boxheight/itemlength+1);
-	$('#myCarousel .list-group-item').outerHeight(triggerheight);
-});
+// $(document).ready(function(){
+//     var clickEvent = false;
+// 	$('#myCarousel').carousel({
+// 		interval:   4000	
+// 	}).on('click', '.list-group li', function() {
+// 			clickEvent = true;
+// 			$('.list-group li').removeClass('active');
+// 			$(this).addClass('active');		
+// 	}).on('slid.bs.carousel', function(e) {
+// 		if(!clickEvent) {
+// 			var count = $('.list-group').children().length -1;
+// 			var current = $('.list-group li.active');
+// 			current.removeClass('active').next().addClass('active');
+// 			var id = parseInt(current.data('slide-to'));
+// 			if(count == id) {
+// 				$('.list-group li').first().addClass('active');	
+// 			}
+// 		}
+// 		clickEvent = false;
+// 	});
+// });
+// $(window).load(function() {
+//     var boxheight = $('#myCarousel .carousel-inner').innerHeight();
+//     var itemlength = $('#myCarousel .item').length;
+// 	var triggerheight = Math.round(boxheight/itemlength+1);
+// 	$('#myCarousel .list-group-item').outerHeight(triggerheight);
+// });
 
 angular.module('ng-dashboard-app').directive("ngHomeView", function () {
     return {
@@ -36,32 +36,38 @@ angular.module('ng-dashboard-app').directive("ngHomeView", function () {
 
 angular.module('ng-dashboard-app').controller('ng-home-view-ctrl', function ($scope, $timeout, $http) {
     
-    $scope.inLoadingData = false;
-    $scope.platformList = [
-        {name: 'Windows Store', index: '1'},
-        {name: 'Windows Phone', index: '2'},
-        {name: 'Android', index: '3'}
-        ];
+    $scope.timeCountDown = {
+        issue: 2008072,
+        dueTime: new Date("2018/06/26 21:15"),
+        restTime: '00天 00时 00分 00秒'
+    };
 
-    $scope.scopeList = [
-        {name: 'in one day', scope: '1'},
-        {name: 'in one week', scope: '7'},
-        {name: 'in one month', scope: '30'}
-        ];
+    function startTime() {
+        var today = new Date();
 
-    $scope.selectedPlatform = $scope.platformList[0];
-    $scope.selectedScope = $scope.scopeList[0];
-    $scope.showNotificationView = false;
+        var diff = $scope.timeCountDown.dueTime.getTime() - today.getTime();
 
-    $scope.refreshTable = function() {
+        var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        diff -=  days * (1000 * 60 * 60 * 24);
         
-        if ($scope.selectedPlatform !== undefined && $scope.selectedScope !== undefined) {
-            $scope.inLoadingData = true;
-            $http.get('/users/?platform=' + $scope.selectedPlatform.index + '&scope=' + $scope.selectedScope.scope).success(function (res) {
-               $scope.userList = res.data;
-               $scope.countOfResult = $scope.userList.length; 
-               $scope.inLoadingData = false;
-            });
-        }
-    }  
+        var hours = Math.floor(diff / (1000 * 60 * 60));
+        diff -= hours * (1000 * 60 * 60);
+        
+        var mins = Math.floor(diff / (1000 * 60));
+        diff -= mins * (1000 * 60);
+        
+        var seconds = Math.floor(diff / (1000));
+        diff -= seconds * (1000);
+
+        $scope.timeCountDown.restTime = checkTime(days) + '天 ' + checkTime(hours) + '时 ' + checkTime(mins) + '分 ' + checkTime(seconds) + '秒';
+
+        var t = $timeout(startTime, 1000);
+    }
+
+    function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
+
+    startTime();
 });
