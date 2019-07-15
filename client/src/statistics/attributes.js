a6d2eaf95235ac71eeb11fde5409e5558568cbed
Michael-Zengxu Zhang <Zengxu.Zhang@autodesk.com>
2019-07-16 00:41:32 +0800

    Define attributes page

--- client/src/statistics/attributes.js
+++ client/src/statistics/attributes.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
-import { Timeline, Badge, Spin} from 'antd';
+import { Divider, List, Icon, Card, Collapse, Button, Spin, Progress } from 'antd';
 import AipHelper from './api-provider';
 import './attributes.css';
+
+const { Panel } = Collapse;
+const { Meta } = Card;
 
 class Attributes extends Component {
 
@@ -9,8 +12,18 @@ class Attributes extends Component {
         super(props);
 
         this.state = {
-            Attributes: null,
+            Attributes: {},
+            Recommendation: [],
             loading: false,
+            Filters: [
+                0, // HitCount
+                0, // HitProbability
+                -100, // IdealProbility
+                -100, // AverageOmission
+                0, // MaxOmission
+                0, // ImmediateOmission
+                8, // ProtentialEnergy
+            ]
         };
     }
 
@@ -19,23 +32,45 @@ class Attributes extends Component {
     }
 
     LoadAttributes = () => {
-        if (!this.state.Attributes) {
-            this.setState({
-                loading: true,
-            });
+        this.setState({
+            loading: true,
+        });
 
-            AipHelper.getAttributes((res) => {
-                if (res) {
-                    this.setState({
-                        Attributes: res,
-                    });
-                }
-
+        AipHelper.getAttributes((res) => {
+            if (res) {
+                // get recommandataion according to the settings
                 this.setState({
-                    loading: false,
+                    Attributes: res,
+                    Recommendation: this.GetRecommandation(res.Categories)
+                });
+            }
+
+            this.setState({
+                loading: false,
+            });
+        });
+    }
+
+    GetRecommandation = (categories) => {
+        let states = [];
+        categories.forEach(cat => {
+            cat.Attributes.forEach(attri => {
+                attri.States.forEach(state => {
+                    let matrix = state.Value.split(',');
+                    if (!matrix.find((val, inx) => val < this.state.Filters[inx])) {
+                        states.push({
+                            Display: attri.Display,
+                            Expression: state.Expression,
+                            AverageOmission: matrix[3],
+                            ImmediateOmission: matrix[5],
+                            ProtentialEnergy: matrix[6],
+                        })
+                    }
                 });
             });
-        }
+        });
+
+        return states.sort((a, b) => b.ProtentialEnergy - a.ProtentialEnergy);
     }
 
     ShowAttributeDetail = (attri) => {
@@ -43,11 +78,68 @@ class Attributes extends Component {
     }
 
     render() {
+        let { Attributes, Recommendation } = this.state;
+        let categories = Attributes.Categories || [];
 
         return (
             <div className="attributes-container">
+                <Divider>异常属性</Divider>
                 <Spin spinning={this.state.loading} size="large">
+                    {
+                        Recommendation.map(attri => {
+                            return <Card className='recommandation-card' key={attri.Display}>
+                                <Meta
+                                    avatar={
+                                        <Progress
+                                            strokeColor={{
+                                                '0%': 'rgb(150, 0, 0)',
+                                                '50%': 'rgb(200, 0, 0)',
+                                                '100%': 'rgb(255, 0, 0)',
+                                            }}
+                                            strokeWidth={15}
+                                            strokeLinecap='square'
+                                            format={percent => `${percent}`}
+                                            percent={Math.round(Math.min(attri.ProtentialEnergy, 14.9) * 100 / 15)}
+                                            type="dashboard"
+                                            width={60}
+                                        />
+                                    }
+                                    title={`${attri.Display} = ${attri.Expression}`}
+                                    description={`平均遗漏：${attri.AverageOmission}期 当前遗漏：${attri.ImmediateOmission}期`}
+                                />
+                            </Card>
+                        })
+                    }
                 </Spin>
+                <Divider>全部属性</Divider>
+                {
+                    categories.map(cat => {
+                        return <Collapse
+                            key={cat.Name}
+                            expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}
+                        >
+                            <Panel className='category-panel' header={cat.Display}>
+                                <List
+                                    grid={{
+                                        gutter: 3,
+                                        xs: 2,
+                                        sm: 3,
+                                        md: 4,
+                                        lg: 4,
+                                        xl: 6,
+                                        xxl: 6,
+                                    }}
+                                    dataSource={cat.Attributes}
+                                    renderItem={attri => (
+                                        <List.Item>
+                                            <Button>{attri.Display}</Button>
+                                        </List.Item>
+                                    )}
+                                />
+                            </Panel>
+                        </Collapse>
+                    })
+                }
             </div>
         );
     }
